0af61bdf27fcf57639bfa08e6d75b3b2729f6401
GULLAPALLI JYOTHI KIRAN <gullapallihemalatha12@gmail.com>
2022-12-08 21:53:55 +0530

    working version

---
+++ colorspinner/app.js
@@ -0,0 +1,19 @@
+//color spinner js code for hex color
+const button=document.getElementById('button-color');
+button.addEventListener('click',mycolor);
+console.log(button);
+function mycolor(){
+    const text=document.getElementById('js-text');
+    const colorcode='0123456789ABCDEF';
+    let hexsign='#';
+    for(let i= 0;i<6;i++){
+        var letter=Math.floor(Math.random()*16);
+        hexsign=hexsign+colorcode[letter];
+    }
+    let full=hexsign;
+    console.log(full)
+    document.getElementById('backgroundcolor').style.backgroundColor=full;
+    document.getElementById('heading-color').style.color=full;
+    text.innerText=full;
+        
+}
